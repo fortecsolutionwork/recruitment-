1f4615ccfbbddda67ffa1238f18080be4644c8ed
ortecsolutionwork <fortecsolutionwork@gmail.com>
2021-11-10 18:18:25 +0530

    set slider

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,16 @@
+
+// header
+
+$(function () {
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > 50) {
+      $("header").addClass("fixed");
+    } else {
+      $("header").removeClass("fixed");
+    }
+  });
+});
+
 $(".testimonials-slider").slick({
   dots: true,
   arrows:false,
